1a77e6a9bcade3d601575d6e4bb66011fe899318
Karim <karimentwickler@gmail.com>
2026-01-09 11:34:30 +0100

    wire TodoService into TodoApp and display tasks

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -2,13 +2,23 @@
 /**
  * Entry point of the TODO application.
  *
- * This class is responsible for starting the application.
- * For now, it only prints a startup message.
- * Later, it will handle user interaction.
+ * This class connects the application logic (TodoService)
+ * and displays tasks in the console.
  */
 
 public class TodoApp {
     public static void main(String[] args) {
-        System.out.println("TODO App started");
+        // Create the service responsible for task management
+        TodoService service = new TodoService();
+
+        // Add sample tasks
+        service.addTask("Learn Java basics");
+        service.addTask("Build TODO application");
+
+        // Display all tasks
+        for (Task task : service.getTasks()) {
+            System.out.println(task);
+        }
+
     }
 }
