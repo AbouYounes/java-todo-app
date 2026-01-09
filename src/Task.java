1d63fe25c11570d048a00ed3e61796bab92347aa
Karim <karimentwickler@gmail.com>
2026-01-09 11:11:07 +0100

    Add Task model

---
+++ src/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private int id;
+    private String description;
+    private boolean completed;
+
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+        this.completed = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markCompleted() {
+        this.completed = true;
+    }
+
+    @Override
+    public String toString() {
+        return id + ". " + description +
+                (completed ? " [DONE]" : " [TODO]");
+    }
+
+}
